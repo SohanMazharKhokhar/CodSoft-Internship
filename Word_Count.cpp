fb8a266011558e4e435a2dfe39ad1543c2ca5cbe
SohanMazharKhokhar <141278073+SohanMazharKhokhar@users.noreply.github.com>
2023-08-15 21:35:06 +0500

    Update Word_Count.cpp

--- Word_Count.cpp
+++ Word_Count.cpp
@@ -4,25 +4,37 @@
 #include <string>
 using namespace std;
 int Word_count() {
-    ofstream fout;
     ifstream fin;
-    string arr;
     string file_name;
-    int word_count = 1;
-    cout<<"Please Enter a File Name\n";
-    cin>>file_name;
+    string arr;
+    int word_count = 1; 
+    while(1){
+    cout << "Please Enter a File Name\n";
+    cin >> file_name;
     fin.open(file_name);
+
     if (!fin.is_open()) {
         cout << "File is not opened" << endl;
         return 1;
     }
-    while (fin >> arr) {
-        word_count++;
+    while (getline(fin, arr)) {
+        string words = "";
+        
+        for (char character : arr) {
+            if (character != ' ' && character != '\t') {
+                words += character;
+            } else if (!words.empty()) {
+                words = "";
+                word_count++;
+            }
+        }
     }
     fin.close();
-    cout << "Total word count from a file is\t" <<  word_count << endl;
-    return 0;
+    cout << "Total word count from a file is\t" << word_count << endl;
+    }
+    return 0; 
 }
+
 int main(){
    //This funtion will count words from a given file after taking a file name from user
    Word_count();
